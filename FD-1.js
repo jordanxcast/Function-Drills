4f8b10a2c171d3b04e0c3d594d17cd6122454d21
Jordan Castillo <jordanxcallaway@gmail.com>
2019-12-02 13:45:21 -0800

    created getYOB function

--- FD-1.js
+++ FD-1.js
@@ -1,6 +1,9 @@
 // eslint-disable-next-line strict
 function createGreeting(name, age) {
   return `Hi, my name is ${name} and I'm ${age} years old`;
+}
+
+function getYearOfBirth(age){
   const yearOf = 2019 - age;
   return `I was born in ${yearOf}`;
 }
@@ -8,3 +11,4 @@ function createGreeting(name, age) {
 const greeting1 = createGreeting();
 // eslint-disable-next-line no-console
 console.log(greeting1);
+
