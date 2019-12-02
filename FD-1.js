f13470c51935037482b772c8061b2eaba57ee112
Jordan Castillo <jordanxcallaway@gmail.com>
2019-12-02 13:13:27 -0800

    wrapped in a function

--- FD-1.js
+++ FD-1.js
@@ -1,2 +1,7 @@
 // eslint-disable-next-line strict
-console.log('Hi, my name is Jordan and I\'m 23 years old');
+function createGreeting {
+  console.log('Hi, my name is Jordan and I\'m 23 years old');
+}
+
+createGreeting();
+
