26f855e4e17bb3d8ba948ae0b380c09196a9bca6
Jordan Castillo <jordanxcallaway@gmail.com>
2019-12-02 13:34:58 -0800

    Added the yearOf variable

--- FD-1.js
+++ FD-1.js
@@ -1,8 +1,10 @@
 // eslint-disable-next-line strict
 function createGreeting(name, age) {
   return `Hi, my name is ${name} and I'm ${age} years old`;
+  const yearOf = 2019 - age;
+  return `I was born in ${yearOf}`;
 }
 
 const greeting1 = createGreeting();
+// eslint-disable-next-line no-console
 console.log(greeting1);
-
